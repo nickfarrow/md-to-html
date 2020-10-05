f426bb14a2eeda4580c051cc2ef7d3fea266a5e0
NicholasFarrow <nicholas.w.farrow@gmail.com>
2020-10-05 23:51:02 +1100

    initial creation

---
+++ convert.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/sh
+
+## Template should include more markers for jekyll info..
+# e.g. Title, Tags
+# Also include any extra head tags
+
+if [ ! -d $1 ] || [ "$1" = "" ]
+then
+    echo "Invalid directory..."
+fi
+
+# Recurisvely find all markdown files
+
+
+# Loop over files
+
+    # Make a copy of the HTMl template
+
+    # Open file contents
+    
+    # Extract Jekyll details and write to template if they exist
+    
+    # Replace ```shell with ```python
+
+    # Use markdownb to convert to html
+
+    # Write html to template copy
+
